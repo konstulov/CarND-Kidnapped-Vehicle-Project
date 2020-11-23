67ca109439e7fde0e39863a60bee918ff34524c6
konstulov <konstulov@gmail.com>
2020-11-23 13:16:13 -0800

    Fix bugs in particle_filter.cpp

--- src/particle_filter.h
+++ src/particle_filter.h
@@ -63,8 +63,8 @@ class ParticleFilter {
    * @param predicted Vector of predicted landmark observations
    * @param observations Vector of landmark observations
    */
-  void dataAssociation(std::vector<LandmarkObs> predicted, 
-                       std::vector<LandmarkObs>& observations);
+  std::vector<LandmarkObs> dataAssociation(const std::vector<LandmarkObs>& predicted,
+                                           const std::vector<LandmarkObs>& observations);
   
   /**
    * updateWeights Updates the weights for each particle based on the likelihood
